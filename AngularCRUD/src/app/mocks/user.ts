0bd6b2276235db2ae8a77e884d29f0a8fd807949
Wilson Calixto Cordovil Junior <wilson.calixto@dbccompany.com.br>
2023-05-21 12:32:12 -0300

    refactoring the code

---
+++ AngularCRUD/src/app/mocks/user.ts
@@ -0,0 +1,11 @@
+import { User } from "../models/User";
+
+export const mockUser: User = {
+  id: 1,
+  firstName: "fsdfdsferer23123",
+  lastName: "54435435",
+  email: "4rwetrwetretre@dsd.com",
+  address: "432432 rerwer3",
+  phone: "8989888898",
+  createdAt: "1684338987602",
+};
