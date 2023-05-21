d5c54129f9b7e9925fd13e157b9dc83ab60a40d5
Wilson Calixto Cordovil Junior <wilson.calixto@dbccompany.com.br>
2023-05-21 13:59:35 -0300

    adding new unit tests

--- AngularCRUD/src/app/home/home.component.spec.ts
+++ AngularCRUD/src/app/home/home.component.spec.ts
@@ -1,158 +1,149 @@
-// import { TestBed, ComponentFixture } from '@angular/core/testing';
-// import { Router } from '@angular/router';
-// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-// import { ToastrService } from 'ngx-toastr';
-// import { of, throwError } from 'rxjs';
+import {
+  TestBed,
+  ComponentFixture,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { of, throwError } from 'rxjs';
 
-// import { HomeComponent, NgModalConfirm } from './home.component';
-// import { HttpProviderService } from '../service/http-provider.service';
-// import { User } from '../models/User';
+import { HomeComponent, NgModalConfirm } from './home.component';
+import { HttpProviderService } from '../service/http-provider.service';
+import { mockUser } from '../mocks/user';
 
-// describe('HomeComponent', () => {
-//   let component: HomeComponent;
-//   let fixture: ComponentFixture<HomeComponent>;
-//   let mockRouter: any;
-//   let mockModalService: any;
-//   let mockToastrService: any;
-//   let mockHttpProviderService: any;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+  let mockRouter: any;
+  let mockModalService: any;
+  let mockToastrService: any;
+  let mockHttpProviderService: any;
 
-//   const mockUser: User = {
-//     id: 1,
-//     firstName: 'fsdfdsferer23123',
-//     lastName: '54435435',
-//     email: '4rwetrwetretre@dsd.com',
-//     address: '432432 rerwer3',
-//     phone: '8989888898',
-//     createdAt: '1684338987602',
-//   };
+  beforeEach(async () => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate'),
+    };
 
-//   beforeEach(async () => {
-//     mockRouter = {
-//       navigate: jasmine.createSpy('navigate'),
-//     };
+    mockModalService = {
+      open: jasmine.createSpy('open').and.returnValue({
+        result: Promise.resolve('Ok click'),
+      }),
+    };
 
-//     mockModalService = {
-//       open: jasmine.createSpy('open').and.returnValue({
-//         result: Promise.resolve('Ok click'),
-//       }),
-//     };
+    mockToastrService = {
+      success: jasmine.createSpy('success'),
+      error: jasmine.createSpy('error'),
+    };
 
-//     mockToastrService = {
-//       success: jasmine.createSpy('success'),
-//       error: jasmine.createSpy('error'),
-//     };
+    mockHttpProviderService = {
+      getAllUsers: jasmine
+        .createSpy('getAllUsers')
+        .and.returnValue(of({ Users: [] })),
+      deleteUserById: jasmine
+        .createSpy('deleteUserById')
+        .and.returnValue(
+          of({ status: true, message: 'user deleted successfully' })
+        ),
+    };
 
-//     mockHttpProviderService = {
-//       getAllUser: jasmine
-//         .createSpy('getAllUser')
-//         .and.returnValue(of({ Users: [] })),
-//       deleteUserById: jasmine
-//         .createSpy('deleteUserById')
-//         .and.returnValue(
-//           of({ status: true, message: 'user deleted successfully' })
-//         ),
-//     };
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent, NgModalConfirm],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: NgbModal, useValue: mockModalService },
+        { provide: ToastrService, useValue: mockToastrService },
+        { provide: HttpProviderService, useValue: mockHttpProviderService },
+      ],
+    }).compileComponents();
+  });
 
-//     await TestBed.configureTestingModule({
-//       declarations: [HomeComponent, NgModalConfirm],
-//       providers: [
-//         { provide: Router, useValue: mockRouter },
-//         { provide: NgbModal, useValue: mockModalService },
-//         { provide: ToastrService, useValue: mockToastrService },
-//         { provide: HttpProviderService, useValue: mockHttpProviderService },
-//       ],
-//     }).compileComponents();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HomeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   xit('should create the component', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should retrieve all users', () => {
+    const mockData = { Users: [] };
+    mockHttpProviderService.getAllUsers.and.returnValue(of(mockData));
 
-//   xit('should retrieve all users', () => {
-//     const mockData = { Users: [] };
-//     mockHttpProviderService.getAllUser.and.returnValue(of(mockData));
+    component.getAllUsers();
 
-//     component.getAllUsers();
+    expect(mockHttpProviderService.getAllUsers).toHaveBeenCalled();
+    expect(component.userList).toEqual(mockData.Users);
+  });
 
-//     expect(mockHttpProviderService.getAllUser).toHaveBeenCalled();
-//     expect(component.userList).toEqual(mockData.Users);
-//   });
+  it('should handle error when retrieving users', () => {
+    const mockError = {
+      status: 404,
+      error: { message: 'Users not found' },
+    };
+    mockHttpProviderService.getAllUsers.and.returnValue(throwError(mockError));
 
-//   xit('should handle error when retrieving users', () => {
-//     const mockError = {
-//       status: 404,
-//       error: { message: 'Users not found' },
-//     };
-//     mockHttpProviderService.getAllUser.and.returnValue(throwError(mockError));
+    component.getAllUsers();
 
-//     component.getAllUsers();
+    expect(mockHttpProviderService.getAllUsers).toHaveBeenCalled();
+    expect(component.userList).toEqual([]);
+  });
 
-//     expect(mockHttpProviderService.getAllUser).toHaveBeenCalled();
-//     expect(component.userList).toEqual([]);
-//   });
+  it('should navigate to AddUser page', () => {
+    component.AddUser();
 
-//   xit('should navigate to AddUser page', () => {
-//     component.AddUser();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['AddUser']);
+  });
 
-//     expect(mockRouter.navigate).toHaveBeenCalledWith(['AddUser']);
-//   });
+  it('should open NgModalConfirm and delete user on deleteUserConfirmation', fakeAsync(() => {
+    mockModalService.open.and.returnValue({
+      result: Promise.resolve('Ok click'),
+    });
+    spyOn(component, 'deleteUser');
 
-//   xit('should open delete confirmation modal and delete user on confirmation', async () => {
-//     mockModalService.open.and.returnValue({
-//       result: Promise.resolve('Ok click'),
-//     });
+    component.deleteUserConfirmation(mockUser);
+    tick();
 
-//     await component.deleteUserConfirmation(mockUser);
+    expect(mockModalService.open).toHaveBeenCalledWith(NgModalConfirm, {
+      ariaLabelledBy: 'modal-basic-title',
+    });
+    expect(component.deleteUser).toHaveBeenCalledWith(mockUser);
+  }));
 
-//     expect(mockModalService.open).toHaveBeenCalledWith(NgModalConfirm, {
-//       ariaLabelledBy: 'modal-basic-title',
-//     });
-//     expect(mockHttpProviderService.deleteUserById).toHaveBeenCalledWith(
-//       mockUser.id
-//     );
-//     expect(mockToastrService.success).toHaveBeenCalledWith(
-//       'user deleted successfully'
-//     );
-//     expect(component.getAllUser).toHaveBeenCalled();
-//   });
+  xit('should open delete confirmation modal and not delete user on cancellation', fakeAsync(() => {
+    mockModalService.open.and.returnValue({
+      result: Promise.reject('cancel click'),
+    });
+    spyOn(component, 'deleteUser');
 
-//   xit('should open delete confirmation modal and not delete user on cancellation', async () => {
-//     mockModalService.open.and.returnValue({
-//       result: Promise.reject('cancel click'),
-//     });
+    component.deleteUserConfirmation(mockUser);
+    tick();
 
-//     await component.deleteUserConfirmation(mockUser);
+    expect(mockModalService.open).toHaveBeenCalledWith(NgModalConfirm, {
+      ariaLabelledBy: 'modal-basic-title',
+    });
 
-//     expect(mockModalService.open).toHaveBeenCalledWith(NgModalConfirm, {
-//       ariaLabelledBy: 'modal-basic-title',
-//     });
-//     expect(mockHttpProviderService.deleteUserById).not.toHaveBeenCalled();
-//     expect(mockToastrService.success).not.toHaveBeenCalled();
-//     expect(component.getAllUser).not.toHaveBeenCalled();
-//   });
+    expect(mockHttpProviderService.deleteUserById).not.toHaveBeenCalled();
+    expect(mockToastrService.success).not.toHaveBeenCalled();
+    expect(component.getAllUsers).not.toHaveBeenCalled();
+  }));
 
-//   xit('should handle error when deleting user', () => {
-//     const mockError = {
-//       message: 'An error occurred while deleting the user',
-//     };
-//     mockHttpProviderService.deleteUserById.and.returnValue(
-//       throwError(mockError)
-//     );
+  it('should handle error when deleting user', () => {
+    const mockError = {
+      message: 'Operation error',
+    };
+    mockHttpProviderService.deleteUserById.and.returnValue(
+      throwError(mockError)
+    );
 
-//     component.deleteUser(mockUser);
+    component.deleteUser(mockUser);
 
-//     expect(mockHttpProviderService.deleteUserById).toHaveBeenCalledWith(
-//       mockUser.id
-//     );
-//     expect(mockToastrService.error).toHaveBeenCalledWith(
-//       'An error occurred while deleting the user'
-//     );
-//     expect(component.getAllUser).not.toHaveBeenCalled();
-//   });
-// });
+    expect(mockHttpProviderService.deleteUserById).toHaveBeenCalledWith(
+      mockUser.id
+    );
+    expect(mockToastrService.error).toHaveBeenCalledWith('Operation error');
+  });
+});
